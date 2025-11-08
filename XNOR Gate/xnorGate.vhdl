8bb6bef76fbe71f3ae207f94299484291ebf2e1e
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-08 21:52:37 +0300

    Add files via upload

---
+++ XNOR Gate/xnorGate.vhdl
@@ -0,0 +1,13 @@
+LIBRARY ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY xnor_gate is
+    port(x,y: in std_logic;
+         z: out std_logic);
+end xnor_gate;
+
+
+ARCHITECTURE behavioral of xnor_gate is
+    begin
+        z<= x XNOR y;
+    end xnor_gate
