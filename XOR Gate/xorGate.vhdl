bee0e3124f06e0b7fd8fcb5f2d96e9c732f4d91a
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-08 21:46:45 +0300

    Add files via upload

---
+++ XOR Gate/xorGate.vhdl
@@ -0,0 +1,13 @@
+LIBRARY ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY xor_gate is
+    port(x,y: in std_logic;
+         z: out std_logic);
+end xor_gate;
+
+
+ARCHITECTURE behavioral of xor_gate is
+    begin 
+        z<= x XOR y;
+end xor_gate;
